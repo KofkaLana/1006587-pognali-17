75d9269b1814727a78e908e62d6948183d64e6fb
Svetlana <orrisa@yandex.ru>
2019-08-27 17:55:19 +0300

    Добавила скрипт открытия/закрытия меню

---
+++ source/js/menu.js
@@ -0,0 +1,16 @@
+var navMain = document.querySelector('.page-header');
+var navToggle = document.querySelector('.page-header__toggle');
+
+navMain.classList.remove('page-header--nojs');
+navMain.classList.remove('page-header--opened');
+navMain.classList.add('page-header--closed');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('page-header--closed')) {
+    navMain.classList.remove('page-header--closed');
+    navMain.classList.add('page-header--opened');
+  } else {
+    navMain.classList.add('page-header--closed');
+    navMain.classList.remove('page-header--opened');
+  }
+});
